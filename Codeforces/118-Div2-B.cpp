6529f09567bdd36dc0764801ae094176394b49c6
ccruzc240800 <43286922+ccruzc240800@users.noreply.github.com>
2019-02-17 18:56:26 -0500

    ad hoc

---
+++ Codeforces/118-Div2-B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	for(int i = -n; i <= n; i++) {
+		for(int j = -n; j <= n-abs(i); j++) {
+			int m = abs(i) + abs(j);
+			if(m <= n)	cout<<abs(m-n);
+			else cout<<" ";
+			if(j != n-abs(i)) cout<<" ";
+		}
+		cout<<endl;
+	}
+}
